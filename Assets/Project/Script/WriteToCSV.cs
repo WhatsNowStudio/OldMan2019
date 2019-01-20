97ef8e6d233a355ca9ce517a8062879bfb17e101
DESKTOP-FVSCF23\htw75 <htw750128@gmail.com>
2019-01-20 23:38:32 +0800

    #第一次上傳

---
+++ Assets/Project/Script/WriteToCSV.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class WriteToCSV : MonoBehaviour {
+	public string filePath = "";
+	string _c = "";
+
+	public void ClearData() {
+		_c = "";
+	}
+	
+	public void ImportData(string c) {
+		_c += c + "\r\n";
+	}
+	
+	public void SaveData(string f) {
+		filePath = Path.Combine (GetAndExternalStorageDirectory (), "OldmanVR/" + f);
+		string d = Path.GetDirectoryName (filePath);
+		if (Directory.Exists (d) == false)  Directory.CreateDirectory (d);
+		if (File.Exists (filePath)) {
+			File.SetAttributes (filePath, FileAttributes.Normal);
+			File.Delete (filePath);
+		}
+		File.AppendAllText (filePath, _c, System.Text.Encoding.UTF8);
+		if (Directory.Exists(f)) Application.OpenURL(f);
+	}
+	
+	string GetAndExternalStorageDirectory()
+	{
+		return Application.dataPath;
+	}
+}
