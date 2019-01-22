2a35f7ab0c068dc1bd032f9b28d13823e735dabb
DESKTOP-FVSCF23\htw75 <htw750128@gmail.com>
2019-01-22 10:10:12 +0800

    更新很多

--- Assets/Project/Script/WriteToCSV.cs
+++ Assets/Project/Script/WriteToCSV.cs
@@ -4,30 +4,60 @@ using System.IO;
 
 public class WriteToCSV : MonoBehaviour {
 	public string filePath = "";
-	string _c = "";
+	//public GameObject goFilePath;
+	string _content = "";
+
+	/*
+	public void test () {
+		ClearData ();
+		for (int i=0; i<30; i++) {
+			ImportData (string.Format ("Name: {0}, Score: {1}", i, (new System.Random ()).Next () % 100));
+		}
+		SaveData (string.Format ("WriteToCSV_{0}.csv", System.DateTime.Now.ToString ("yyyyMMddHHmmss")));
+		Debug.Log (filePath);
+		if (goFilePath != null) {
+			goFilePath.GetComponent<UnityEngine.UI.Text> ().text = filePath;
+		}
+	}
+	*/
 
 	public void ClearData() {
-		_c = "";
+		_content = "";
 	}
 	
-	public void ImportData(string c) {
-		_c += c + "\r\n";
+	public void ImportData(string content) {
+		_content += content + "\r\n";
 	}
 	
-	public void SaveData(string f) {
-		filePath = Path.Combine (GetAndExternalStorageDirectory (), "OldmanVR/" + f);
-		string d = Path.GetDirectoryName (filePath);
-		if (Directory.Exists (d) == false)  Directory.CreateDirectory (d);
+	public void SaveData(string fileName) {
+		filePath = Path.Combine (GetAndExternalStorageDirectory (), "OldMan2019/" + fileName);
+		if (Application.platform == RuntimePlatform.Android) {
+			filePath = filePath.Replace (@"\", "/");
+		}
+		string folderPath = Path.GetDirectoryName (filePath);
+		if (Directory.Exists (folderPath) == false) {
+			Directory.CreateDirectory (folderPath);
+		}
 		if (File.Exists (filePath)) {
 			File.SetAttributes (filePath, FileAttributes.Normal);
 			File.Delete (filePath);
 		}
-		File.AppendAllText (filePath, _c, System.Text.Encoding.UTF8);
-		if (Directory.Exists(f)) Application.OpenURL(f);
+		//File.AppendAllText (filePath, _content);
+		File.AppendAllText (filePath, _content, System.Text.Encoding.UTF8);
 	}
 	
 	string GetAndExternalStorageDirectory()
 	{
-		return Application.dataPath;
+		string path = "";
+		if (Application.platform == RuntimePlatform.Android) {
+			AndroidJavaClass androidEnvironment = new AndroidJavaClass ("android.os.Environment");
+			using (AndroidJavaObject externalStorageDirectory = androidEnvironment.CallStatic<AndroidJavaObject>("getExternalStorageDirectory")) {
+				path = externalStorageDirectory.Call<string> ("getPath");
+			}
+		} else {
+			//path = Path.GetTempPath();
+			path = Application.persistentDataPath;
+		}
+		return path;
 	}
 }
